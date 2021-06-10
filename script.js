c1c62ad502168ef05c3bb62926a78da15adee880
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-10 18:01:19 +0530

    Back to initial set-up

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const todoList = document.querySelectorAll('.task');
-console.log(todoList);
+// const todoList = document.querySelectorAll('.task');
+// console.log(todoList);
 
-todoList.forEach((listTask) => {
-    listTask.addEventListener('click', () => {
-        makeStrike(listTask);
-        console.log('Clicked');
-    });
-});
+// todoList.forEach((listTask) => {
+//     listTask.addEventListener('click', () => {
+//         makeStrike(listTask);
+//         console.log('Clicked');
+//     });
+// });
 
 
-function makeStrike(elem) {
-    let status = elem.className;
-    console.log(status);
-    if (status.includes('strike')) {
-        elem.classList.remove('strike');
-        return;
-    }
-    elem.classList.add('strike');
-}
+// function makeStrike(elem) {
+//     let status = elem.className;
+//     console.log(status);
+//     if (status.includes('strike')) {
+//         elem.classList.remove('strike');
+//         return;
+//     }
+//     elem.classList.add('strike');
+// }
