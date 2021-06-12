984f251231cc6e0027b5b3fed242969399178314
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-12 21:01:25 +0530

    add and remove todos added

--- script.js
+++ script.js
@@ -18,3 +18,45 @@
 //     }
 //     elem.classList.add('strike');
 // }
+
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todoUl = document.getElementById('todos');
+
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
+    console.log(todoText);
+
+    if(todoText) {
+        const todoEl = document.createElement('li');
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener('click', (ev) => {
+            todoEl.classList.toggle('completed');
+        });
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            todoEl.remove();
+        })
+
+        todoUl.appendChild(todoEl);
+
+        input.value = '';
+    }
+}
+
