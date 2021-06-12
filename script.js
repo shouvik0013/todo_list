6fccd0d5541d7f30b20d82f8364e7dccf90bfced
Shouvik Mandal <shouvik0013@gmail.com>
2021-06-12 21:47:56 +0530

    Full functionality added

--- script.js
+++ script.js
@@ -18,11 +18,23 @@
 //     }
 //     elem.classList.add('strike');
 // }
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+//     document.body.style.width = '300px';
+//   }else{
+//     document.write("not mobile");
+//   }
 
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todoUl = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => {
+        addTodo(todo);
+    });
+}
 
 form.addEventListener('submit', (ev) => {
     ev.preventDefault();
@@ -47,16 +59,34 @@ function addTodo(todo) {
 
         todoEl.addEventListener('click', (ev) => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         })
 
         todoUl.appendChild(todoEl);
 
         input.value = '';
+
+        updateLS();
     }
+}
+
+function updateLS() {
+    let todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach(elem => {
+        todos.push({
+            text: elem.innerText,
+            completed: elem.classList.contains('completed')
+        });
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
